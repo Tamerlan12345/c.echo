944687d3ac2a7481886abbbf7112e16209597ea3
Тамерлан <centr-cool@mail.ru>
2026-05-24 20:19:04 +0300

    fffx

--- apps/web/src/app/dashboard/page.tsx
+++ apps/web/src/app/dashboard/page.tsx
@@ -141,10 +141,10 @@ export default function DashboardPage() {
       <aside className={styles.sidebar}>
         <div className={styles.sidebarLogo}>
           <div className={styles.fullLogo}>
-            <Logo size={26} />
+            <Logo size={40} />
           </div>
           <div className={styles.iconLogo}>
-            <LogoIcon />
+            <LogoIcon width={40} height={40} />
           </div>
         </div>
 
