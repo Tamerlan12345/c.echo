591d91e7e20ba67d23f146a1d3dec02195617b3c
tamerlan12345 <centr-cool@mail.ru>
2026-05-28 16:07:09 +0500

    fix(livekit): remove invalid configuration keys allowed_origins and egress and move redis to root level

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Substitute environment variables from template into /tmp/livekit.yaml safely
-envsubst '$LIVEKIT_TCP_PORT $LIVEKIT_NODE_IP $EXTERNAL_TURN_USERNAME $EXTERNAL_TURN_CREDENTIAL $LIVEKIT_ALLOWED_ORIGINS $LIVEKIT_API_KEY $LIVEKIT_API_SECRET $REDIS_ADDRESS $REDIS_PASSWORD' < /etc/livekit.yaml.template > /tmp/livekit.yaml
+envsubst '$LIVEKIT_TCP_PORT $LIVEKIT_NODE_IP $EXTERNAL_TURN_USERNAME $EXTERNAL_TURN_CREDENTIAL $LIVEKIT_API_KEY $LIVEKIT_API_SECRET $REDIS_ADDRESS $REDIS_PASSWORD' < /etc/livekit.yaml.template > /tmp/livekit.yaml
 
 # Restrict permissions of the generated file for security
 chmod 600 /tmp/livekit.yaml
