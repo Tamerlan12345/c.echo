984bca25a31f69a25866a4b81e01598f45511e76
tamerlan12345 <centr-cool@mail.ru>
2026-05-29 11:07:01 +0500

    fix(livekit): add environment variable defaults in entrypoint script to prevent template syntax crashes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# Set default values for essential port and IP variables if they are missing
+export PORT="${PORT:-8080}"
+export LIVEKIT_TCP_PORT="${LIVEKIT_TCP_PORT:-25037}"
+export LIVEKIT_NODE_IP="${LIVEKIT_NODE_IP:-66.33.22.227}"
+
 # Substitute environment variables from template into /tmp/livekit.yaml safely
 envsubst '$PORT $LIVEKIT_TCP_PORT $LIVEKIT_NODE_IP $EXTERNAL_TURN_USERNAME $EXTERNAL_TURN_CREDENTIAL $LIVEKIT_API_KEY $LIVEKIT_API_SECRET $REDIS_ADDRESS $REDIS_PASSWORD' < /etc/livekit.yaml.template > /tmp/livekit.yaml
 
